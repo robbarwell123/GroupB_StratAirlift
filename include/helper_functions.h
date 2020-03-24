9c5301ca901de3c1bb1d0ca431a2077550313ad1
Myde001 <54612217+Myde001@users.noreply.github.com>
2020-03-23 21:14:20 -0400

    committed the helper functions

---
+++ include/helper_functions.h
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  common_functions
+//
+//  Created by Olumide Akinola on 2020-03-15.
+//  Copyright © 2020 Olumide Akinola. All rights reserved.
+//
+
+#include "datafile.h"
+
+struct AIRPORT** get_2D_array(int *size, char *line, char* fileName);
+char* get_field(char *line, int col);
+int get_continent(char *val);
+void increment(char *val, int *size);
