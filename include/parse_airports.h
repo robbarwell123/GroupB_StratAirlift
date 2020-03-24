c98ebbf3d4f9c77de2c7d54d579ec1b3d102d505
olumideakinola <olumideakinola16@gmail.com>
2020-03-23 21:43:15 -0400

    updated the documentation for parse_airports

--- include/parse_airports.h
+++ include/parse_airports.h
@@ -7,6 +7,15 @@
 //
 
 #include "datafile.h"
+/**
+* @brief calculates the number of airports in our csv file.
+*
+* The 2D array of airports is needed as input in int gen_locations.
 
+*
+* @param my_airports The 2D array of struct for airports.
+* @param airport_file The airport file to be run.
+* @return The value 0 to represent successfully counting the number of airports and -1 represents an error finding the number of airports.
+*/
 
 int parse_airports(struct AIRPORT** my_airports, char *airport_file);
