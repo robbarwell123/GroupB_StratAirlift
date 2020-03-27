c734c8c0bc507507576262c20d692893c6d60c86
Edwin-programmer <60937418+Edwin-programmer@users.noreply.github.com>
2020-03-26 23:47:14 -0400

    Adding main.h and gen_location.h
    changes include the following files:
    include/
        main.h
        strat_airlift_functions.h
        gen_locations.h
        data_types.h

---
+++ include/gen_locations.h
@@ -0,0 +1,8 @@
+#ifndef __GEN_LOCATIONS_H__
+#define __GEN_LOCATIONS_H__
+
+#include "data_types.h"
+
+int check_used(int curr_pos, int curr_rand,int* selected_airports);
+
+#endif // __GEN_LOCATIONS_H__
---
+++ include/main.h
@@ -0,0 +1,6 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+int run_generator(char* config_filename,char* airport_filename,char* output_dir);
+
+#endif // __MAIN_H__
