03a53414d27797594defb346a5ea99c344cd39b8
Edwin-programmer <60937418+Edwin-programmer@users.noreply.github.com>
2020-03-31 02:34:27 -0400

    Added Doxygen comments

--- include/main.h
+++ include/main.h
@@ -1,6 +1,23 @@
+/**
+ * @file main.h
+ * @author Edwin Omoigui
+ * @date 31 Mar 2020
+ * @brief This file contains the functions required for the main file and calling all the sub-functions.
+ */
+
 #ifndef __MAIN_H__
 #define __MAIN_H__
+
+/**
+ * @brief Takes command line arguments for calling and passing functions required for the strategic airlift simulator.
+ *
+ * @param config_filename The user's specified configuration file with default file being the generator.cfg within the directory of execution.
+ * @param airport_filename The user's specified common airport file.
+ * @param output_dir The output directory to be used.
+ * @return The value 0 to represent error while any other values represent successfully calling all functions without any error.
+ */
 
 int run_generator(char* config_filename,char* airport_filename,char* output_dir);
 
 #endif // __MAIN_H__
+
