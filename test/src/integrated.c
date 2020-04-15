883366adefa7490771296b9f31676f1dba84575e
olumideakinola <olumideakinola16@gmail.com>
2020-04-14 23:11:19 -0400

    Update integrated.c

--- test/src/integrated.c
+++ test/src/integrated.c
@@ -13,74 +13,64 @@
 #include "../../include/data_types.h"
 #include "../../include/common_functions.h"
 #include "../../include/strat_airlift_functions.h"
+#include "../include/strat_airlift_functions_tests.h"
 #include "../include/integrated.h"
 
-struct AIRPORT** calc_shortest_path_test_set_airports() {
-    struct AIRPORT **temp_airports;
+**
+ * @brief my_sizes is used to store the sizes of the dynamic arrays used throughout the program.
+ */
+struct SIZES *my_sizes;
+
+int integrated_test(void) {
+    int rtn_val=0;                                      // The current state of the function to return
+    int scenarios=1;                                    // The number of scenarios to generate
+    int curr_scenario=0;                                // The current scenario number
     
-    temp_airports=(struct AIRPORT**)malloc(sizeof(struct AIRPORT*)*7);
+    my_sizes=malloc(sizeof(struct SIZES));
     for(int init=0;init<7;init++){
-        temp_airports[init]=NULL;
+            my_sizes->cont[init]=0;
     }
-    
-    temp_airports[NA]=(struct AIRPORT*)malloc(sizeof(struct AIRPORT));
-    temp_airports[EU]=(struct AIRPORT*)malloc(sizeof(struct AIRPORT)*2);
-    temp_airports[AF]=(struct AIRPORT*)malloc(sizeof(struct AIRPORT));
-    temp_airports[SA]=(struct AIRPORT*)malloc(sizeof(struct AIRPORT));
-    
-    temp_airports[NA][0].id="CYTZ";
-    temp_airports[NA][0].name="CFB Trenton";
-    temp_airports[NA][0].lat=44.1189003;
-    temp_airports[NA][0].lon=-77.52809906;
-    temp_airports[NA][0].cont="NA";
-    temp_airports[NA][0].country="CA";  
+    my_sizes->locations=0;
+    my_sizes->paths=0;
 
-    temp_airports[EU][0].id="EDDK";
-    temp_airports[EU][0].name="Cologne Bonn Airport";
-    temp_airports[EU][0].lat=50.86589813;
-    temp_airports[EU][0].lon=7.142739773;
-    temp_airports[EU][0].cont="EU";
-    temp_airports[EU][0].country="DE";  
+    /* Creates the initial structures which will be used for all function calls. */
+    struct STATE *my_state=malloc(sizeof(struct STATE));
+    struct AIRPORT **my_airports;
+    my_airports=(struct AIRPORT**)malloc(sizeof(struct AIRPORT*)*7);
+    for(int init=0;init<7;init++){
+            my_airports[init]=NULL;
+    }
+    struct PATH *my_paths=NULL;
 
-    temp_airports[EU][1].id="EGPF";
-    temp_airports[EU][1].name="Glasgow International Airport";
-    temp_airports[EU][1].lat=55.871899;
-    temp_airports[EU][1].lon=-4.43306;
-    temp_airports[EU][1].cont="EU";
-    temp_airports[EU][1].country="GB";  
+    if(rtn_val==0 && parse_airports(airport_filename,my_airports)) {
+            rtn_val=-1;
+    }
+    #ifdef DEBUG_ENABLED
+    printf("Airports per Continent\n");
+    for(int print_size=0;print_size<7;print_size++) {
+        printf("Continent %d Airports: %d\n",print_size,my_sizes->cont[print_size]);
+    }
+    #endif
+        
+    /* Release 1 of parse_config uses the default values specified in the requirements document. */
+    if(rtn_val==0 && parse_config(my_airports, config_filename, my_state)!=0) {
+            rtn_val=-2;
+    }
+        
+    /* Updates the number of scenarios to match the config file */
+    scenarios=scenarios<my_state->num_scenarios ? my_state->num_scenarios : scenarios;
 
-    temp_airports[AF][0].id="HKJK";
-    temp_airports[AF][0].name="Jomo Kenyatta International Airport";
-    temp_airports[AF][0].lat=-1.319239974;
-    temp_airports[AF][0].lon=36.92779922;
-    temp_airports[AF][0].cont="AF";
-    temp_airports[AF][0].country="KE";  
+    my_state->airport_list=(struct AIRPORT**)realloc(my_state->airport_list,sizeof(struct AIRPORT*)*9);
 
-    temp_airports[SA][0].id="SKBO";
-    temp_airports[SA][0].name="El Dorado International Airport";
-    temp_airports[SA][0].lat=4.70159;
-    temp_airports[SA][0].lon=-74.1469;
-    temp_airports[SA][0].cont="SA";
-    temp_airports[SA][0].country="CO";  
-    
-    my_sizes->cont[NA]=1;
-    my_sizes->cont[EU]=2;
-    my_sizes->cont[AF]=1;
-    my_sizes->cont[SA]=1;
-    
-    return temp_airports;
-}
-
-void calc_shortest_path_test_set_state(struct AIRPORT** my_airports,struct STATE *my_state){
-    parse_config(my_airports, "./data/blank.cfg", my_state);
-    
-    my_state->airport_list=(struct AIRPORT**)realloc(my_state->airport_list,sizeof(struct AIRPORT*)*5);
-
-    my_state->airport_list[2]=find_airport("EGPF",my_airports);
-    my_state->airport_list[3]=find_airport("HKJK",my_airports);
-    my_state->airport_list[4]=find_airport("SKBO",my_airports);
+    my_state->airport_list[2]=find_airport("DAOF",my_airports);
+    my_state->airport_list[3]=find_airport("EFPO",my_airports);
+    my_state->airport_list[4]=find_airport("FAVR",my_airports);
+    my_state->airport_list[2]=find_airport("KNKT",my_airports);
+    my_state->airport_list[3]=find_airport("SJTC",my_airports);
+    my_state->airport_list[4]=find_airport("SPST",my_airports);
+    my_state->airport_list[4]=find_airport("SPQT",my_airports);
     my_sizes->locations=5;
-    
+
     my_state->distance_matrix=(int**)realloc(my_state->distance_matrix,sizeof(int*)*my_sizes->locations);
     for(int source=0;source<my_sizes->locations;source++) {
         if(source<my_state->num_main_bases){
@@ -91,23 +81,38 @@ void calc_shortest_path_test_set_state(struct AIRPORT** my_airports,struct STATE
         }else{
             my_state->distance_matrix[source]=(int*)calloc(my_sizes->locations,sizeof(int));
         }
-    }    
+    }
+        
+    if(rtn_val==0 && find_closest(my_state)!=0) {
+            rtn_val=-3;
+    }
 
-    // Connection from CYTZ to EDDK
-    my_state->distance_matrix[0][1]=calc_distance(my_state->airport_list[0]->lat,my_state->airport_list[0]->lon,my_state->airport_list[1]->lat,my_state->airport_list[1]->lon);
-    my_state->distance_matrix[1][0]=calc_distance(my_state->airport_list[1]->lat,my_state->airport_list[1]->lon,my_state->airport_list[0]->lat,my_state->airport_list[0]->lon);
+    if(rtn_val==0 && calc_shortest_path(my_state, &my_paths)!=0) {
+            rtn_val=-5;
+    }
 
-    // Connection from CYTZ to SKBO
-    my_state->distance_matrix[0][4]=calc_distance(my_state->airport_list[0]->lat,my_state->airport_list[0]->lon,my_state->airport_list[4]->lat,my_state->airport_list[4]->lon);
-    my_state->distance_matrix[4][0]=calc_distance(my_state->airport_list[4]->lat,my_state->airport_list[4]->lon,my_state->airport_list[0]->lat,my_state->airport_list[0]->lon);
+    if(rtn_val==0){
+        for(int print_loc=0;print_loc<my_state->num_locations;print_loc++){
+            printf("%d,%s,%f,%f\n",print_loc,my_state->airport_list[print_loc]->name,my_state->airport_list[print_loc]->lat,my_state->airport_list[print_loc]->lon);
+        }
+        print_paths(my_paths);
+    }
+        
+        #ifdef DEBUG_ENABLED
+        print_state(my_state);
+        #endif
+        
+        free(my_sizes);
+        free(my_state);
+        free(my_airports);
+        free(my_paths);
 
-    // Connection from EDDK to EGPF
-    my_state->distance_matrix[1][2]=calc_distance(my_state->airport_list[1]->lat,my_state->airport_list[1]->lon,my_state->airport_list[2]->lat,my_state->airport_list[2]->lon);
-    my_state->distance_matrix[2][1]=calc_distance(my_state->airport_list[2]->lat,my_state->airport_list[2]->lon,my_state->airport_list[1]->lat,my_state->airport_list[1]->lon);
-
-    // Connection from EDDK to HKJK
-    my_state->distance_matrix[1][3]=calc_distance(my_state->airport_list[1]->lat,my_state->airport_list[1]->lon,my_state->airport_list[3]->lat,my_state->airport_list[3]->lon);
-    my_state->distance_matrix[3][1]=calc_distance(my_state->airport_list[3]->lat,my_state->airport_list[3]->lon,my_state->airport_list[1]->lat,my_state->airport_list[1]->lon);    
+        if(rtn_val!=0) {
+            break;
+        }
+    }
+    
+    return rtn_val;
 }
 
 int calc_shortest_path_test() {
