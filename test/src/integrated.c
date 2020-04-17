75230ee5225268b36819ce3af40f26b9ae431477
olumideakinola <olumideakinola16@gmail.com>
2020-04-17 00:36:05 -0400

    updated issue 13

--- test/src/integrated.c
+++ test/src/integrated.c
@@ -14,17 +14,15 @@
 #include "../../include/common_functions.h"
 #include "../../include/strat_airlift_functions.h"
 #include "../include/strat_airlift_functions_tests.h"
-#include "../include/integrated.h"
 
-**
+
+/**
  * @brief my_sizes is used to store the sizes of the dynamic arrays used throughout the program.
  */
 struct SIZES *my_sizes;
 
 int integrated_test(void) {
     int rtn_val=0;                                      // The current state of the function to return
-    int scenarios=1;                                    // The number of scenarios to generate
-    int curr_scenario=0;                                // The current scenario number
     
     my_sizes=malloc(sizeof(struct SIZES));
     for(int init=0;init<7;init++){
@@ -42,34 +40,32 @@ int integrated_test(void) {
     }
     struct PATH *my_paths=NULL;
 
-    if(rtn_val==0 && parse_airports(airport_filename,my_airports)) {
+    if(rtn_val==0 && parse_airports("./data/airports.csv",my_airports)) {
             rtn_val=-1;
     }
-    #ifdef DEBUG_ENABLED
+
     printf("Airports per Continent\n");
     for(int print_size=0;print_size<7;print_size++) {
         printf("Continent %d Airports: %d\n",print_size,my_sizes->cont[print_size]);
     }
-    #endif
+
         
     /* Release 1 of parse_config uses the default values specified in the requirements document. */
-    if(rtn_val==0 && parse_config(my_airports, config_filename, my_state)!=0) {
+    if(rtn_val==0 && parse_config(my_airports,"./data/blank.cfg", my_state)!=0) {
             rtn_val=-2;
     }
         
-    /* Updates the number of scenarios to match the config file */
-    scenarios=scenarios<my_state->num_scenarios ? my_state->num_scenarios : scenarios;
 
     my_state->airport_list=(struct AIRPORT**)realloc(my_state->airport_list,sizeof(struct AIRPORT*)*9);
 
     my_state->airport_list[2]=find_airport("DAOF",my_airports);
     my_state->airport_list[3]=find_airport("EFPO",my_airports);
     my_state->airport_list[4]=find_airport("FAVR",my_airports);
-    my_state->airport_list[2]=find_airport("KNKT",my_airports);
-    my_state->airport_list[3]=find_airport("SJTC",my_airports);
-    my_state->airport_list[4]=find_airport("SPST",my_airports);
-    my_state->airport_list[4]=find_airport("SPQT",my_airports);
-    my_sizes->locations=5;
+    my_state->airport_list[5]=find_airport("KNKT",my_airports);
+    my_state->airport_list[6]=find_airport("SJTC",my_airports);
+    my_state->airport_list[7]=find_airport("SPST",my_airports);
+    my_state->airport_list[8]=find_airport("SPQT",my_airports);
+    my_sizes->locations=9;
 
     my_state->distance_matrix=(int**)realloc(my_state->distance_matrix,sizeof(int*)*my_sizes->locations);
     for(int source=0;source<my_sizes->locations;source++) {
@@ -88,82 +84,47 @@ int integrated_test(void) {
     }
 
     if(rtn_val==0 && calc_shortest_path(my_state, &my_paths)!=0) {
-            rtn_val=-5;
+            rtn_val=-4;
     }
 
-    if(rtn_val==0){
-        for(int print_loc=0;print_loc<my_state->num_locations;print_loc++){
-            printf("%d,%s,%f,%f\n",print_loc,my_state->airport_list[print_loc]->name,my_state->airport_list[print_loc]->lat,my_state->airport_list[print_loc]->lon);
-        }
-        print_paths(my_paths);
+    FILE *fp;
+    FILE *fp2;
+    char str[1000];
+    char str2[1000];
+    char* filename = "./data/Locationstxt.xml";
+    char* filename2 = "./data/Locationstxt_copy.xml";
+    int fail = 0;
+    
+    fp = fopen(filename, "r");
+    fp2= fopen(filename2, "r");
+    if (fp == NULL|| fp2 == NULL){
+        printf("Could not open file %s",filename);
+        rtn_val= -5;
+        return rtn_val;
     }
+    while (fgets(str, 1000, fp) != NULL && fgets(str2, 1000, fp2) != NULL){
+        if (strcmp(str,str2)!=0){
+            fail++;
+        }
         
-        #ifdef DEBUG_ENABLED
-        print_state(my_state);
-        #endif
+    }
+    if(fail==0){
+        rtn_val = 0;
+    }else{
+        rtn_val = -6;
+    }
+       
+    fclose(fp);
+    fclose(fp2);
         
-        free(my_sizes);
-        free(my_state);
-        free(my_airports);
-        free(my_paths);
 
-        if(rtn_val!=0) {
-            break;
-        }
-    }
-    
-    return rtn_val;
-}
-
-int calc_shortest_path_test() {
-    int rtn_val=0;                                      // The current state of the function to return
-
-    char *valid_result[]={"0/1 0 1 2 55.87190 -4.43306 1","0/1 0 2 2 55.87190 -4.43306 2","0/1 0 3 3 -1.31924 36.92780 2","0/1 0 4 2 55.87190 -4.43306 2","0/1 1 0 4 4.70159 -74.14690 1","0/1 1 2 4 4.70159 -74.14690 2","0/1 1 3 4 4.70159 -74.14690 2","0/1 1 4 4 4.70159 -74.14690 2","0/1 2 0 0 44.11890 -77.52810 2","0/1 2 1 4 4.70159 -74.14690 2","0/1 2 3 3 -1.31924 36.92780 2","0/1 2 4 4 4.70159 -74.14690 2","0/1 3 0 0 44.11890 -77.52810 2","0/1 3 1 4 4.70159 -74.14690 2","0/1 3 2 2 55.87190 -4.43306 2","0/1 3 4 4 4.70159 -74.14690 2","0/1 4 0 2 55.87190 -4.43306 2","0/1 4 1 1 50.86590 7.14274 2","0/1 4 2 2 55.87190 -4.43306 2","0/1 4 3 3 -1.31924 36.92780 2"};
-    char compare_to[50];
-    
-    /* Creates the initial structures which will be used for all function calls. */
-    struct STATE *my_state=malloc(sizeof(struct STATE));
-    struct AIRPORT **my_airports;
-    struct PATH *my_paths=NULL;
-
-    free(my_sizes);
-    /* Initalizes my_sizes to 0 for all dynamic arrays. */
-    my_sizes=malloc(sizeof(struct SIZES));
-    for(int init=0;init<7;init++){
-        my_sizes->cont[init]=0;
-    }
-    my_sizes->locations=0;
-    my_sizes->paths=0;
-
-    my_airports=calc_shortest_path_test_set_airports();
-    calc_shortest_path_test_set_state(my_airports,my_state);
-
-    #ifdef DEBUG_ENABLED
-    print_state(my_state);
-    #endif
-
-    if(calc_shortest_path(my_state, &my_paths)!=0) {
-        rtn_val=-1;
-    }
-
-    #ifdef DEBUG_ENABLED
-    print_paths(my_paths);
-    #endif
-
-    for(int compare=0;compare<my_sizes->paths;compare++) {
-        sprintf(compare_to,"0/1 %d %d %d %0.5f %0.5f %d",my_paths[compare].source_location,my_paths[compare].dest_location,my_paths[compare].next_location,my_paths[compare].next_lat,my_paths[compare].next_long,my_paths[compare].aircraft_type);
-        if(strcmp(valid_result[compare],compare_to)!=0) {
-            printf("calc_shortest_path test failed on comparison: %d.\n",compare);
-            rtn_val=-2;
-        }
-    }
-    
+        
     free(my_sizes);
     free(my_state);
     free(my_airports);
     free(my_paths);
-
-    my_sizes=NULL;
     
     return rtn_val;
 }
+
+
